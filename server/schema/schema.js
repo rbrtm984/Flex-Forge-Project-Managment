cc2f0e280a3ea33b4c783c4b2b409861991ade0f
Rob Mayo <rbrtm984@gmail.com>
2024-01-02 15:25:36 -0500

    added type for project, projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 // Client Type
@@ -18,9 +19,40 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name: "Project",
+    fields: () => ({
+      id: { type: GraphQLID },
+      name: { type: GraphQLString },
+      description: { type: GraphQLString },
+      status: { type: GraphQLString },
+    }),
+  });
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+        type: new GraphQLList(ProjectType),
+        resolve(parent, args){
+            return projects
+        }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return projects.find((project) => project.id == args.id);
+      },
+    },
+    clients: {
+        type: new GraphQLList(ClientType),
+        resolve(parent, args){
+            return clients
+        }
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
