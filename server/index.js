eaafd29f2a8a949f402d1826d665dd67c8b5aec2
Rob Mayo <rbrtm984@gmail.com>
2024-01-02 15:02:33 -0500

    added sample data and started writing schema

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'develoment'
+    graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`))
