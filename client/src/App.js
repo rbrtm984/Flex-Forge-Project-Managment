d3697e35cfddf3623133b2ff09d08958910fe991
Rob Mayo <rbrtm984@gmail.com>
2024-01-03 18:25:35 -0500

    started building clients component. enabled cors in server. building get clients query in clients component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
+import Clients from "./components/Clients";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-    <Header/>
-      <div className="Container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
