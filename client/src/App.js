d6d362775a5ec4a32018f793c57f89c1ec2849d1
Rob Mayo <rbrtm984@gmail.com>
2024-01-03 17:14:40 -0500

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="Container">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Header/>
+      <div className="Container">
+        <h1>Hello World</h1>
+      </div>
+    </>
   );
 }
 
