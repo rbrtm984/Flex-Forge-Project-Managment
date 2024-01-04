1f2f7cb1f765b58cd8c8102a28ca446d7d9224bd
Rob Mayo <rbrtm984@gmail.com>
2024-01-03 19:05:52 -0500

    started but didnt finish using alternate form of refresh for delete mutation

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -4,19 +4,24 @@ import { GET_CLIENTS } from "../queries/clientQueries";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 export default function ClientRow({ client }) {
-    const [deleteClient] = useMutation(DELETE_CLIENT, {
-        variables: { id: client.id },
-        refetchQueries:[{query: GET_CLIENTS}]
-    });
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter((client) => client.id !== deleteClient.id) },
+      });
+    },
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm"
-        onClick={deleteClient}>
-            <FaTrash />
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
         </button>
       </td>
     </tr>
