55947728fe94985538968abbe423cac797d5514d
Rob Mayo <rbrtm984@gmail.com>
2024-01-06 23:47:23 -0500

    started the addprojectmodal

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -7,6 +7,7 @@ export default function AddClientModal() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
 
   const [addClient] = useMutation(ADD_CLIENT, {
     variables: { name, email, phone },
@@ -21,7 +22,8 @@ export default function AddClientModal() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if(name === "" || email === "" || phone === "") return alert('Please fill out all fields!');
+    if (name === "" || email === "" || phone === "")
+      return alert("Please fill out all fields!");
     addClient(name, email, phone);
     setName("");
     setEmail("");
@@ -32,27 +34,27 @@ export default function AddClientModal() {
     <>
       <button
         type="button"
-        className="btn btn-secondary"
+        className="btn btn-primary"
         data-bs-toggle="modal"
-        data-bs-target="#addClientModal"
+        data-bs-target="#addProjectModal"
       >
         <div className="d-flex align-items-center">
-          <FaUser className="icon" />
-          <div>Add Client</div>
+          <FaList className="icon" />
+          <div>New Project</div>
         </div>
       </button>
       <div
         className="modal fade"
-        id="addClientModal"
-        aria-labelledby="addClientModalLabel"
+        id="addProjectModal"
+        aria-labelledby="addProjectModalLabel"
         aria-hidden="true"
       >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="addClientModalLabel">
-                Add Client
-              </h1>
+              <h5 className="modal-title fs-5" id="addProjectModalLabel">
+                New Project
+              </h5>
               <button
                 type="button"
                 className="btn-close"
@@ -73,29 +75,31 @@ export default function AddClientModal() {
                   />
                 </div>
                 <div className="mb-3">
-                  <label className="form-label">Email</label>
-                  <input
-                    type="email"
+                  <label className="form-label">Description</label>
+                  <textarea
                     className="form-control"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  ></textarea>
                 </div>
                 <div className="mb-3">
-                  <label className="form-label">Phone</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                  />
+                  <label className="form-label">Status</label>
+                  <select
+                    id="status"
+                    className="form-select"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                  >
+                    <option value="new">New</option>
+                    <option value="progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                  </select>
                 </div>
                 <button
                   type="submit"
                   data-bs-dismiss="modal"
-                  className="btn btn-secondary"
+                  className="btn btn-primary"
                 >
                   Submit
                 </button>
